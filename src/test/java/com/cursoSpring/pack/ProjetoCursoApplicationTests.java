caf60e1c5862214d5b3b718a6729b96a58773a20
Gabriel Fonzare <gabrielfonzare@gmail.com>
2026-03-13 20:50:30 -0300

    Projeto criado

---
+++ src/test/java/com/cursoSpring/pack/ProjetoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoSpring.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
